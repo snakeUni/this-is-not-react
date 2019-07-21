b7cc2893a6bb9f115adc87d1144b5acc2cd95d16
Uni <1025687605@qq.com>
2019-07-21 10:27:05 +0800

    change

--- src/my-react.js
+++ src/my-react.js
@@ -1,10 +1,41 @@
 const React = (function() {
+  const hooks = [];
+  const deps = [];
+  let idx = 0;
+  // useState 实现
   function useState(initialValue) {
-    let value = initialValue;
+    let value = hooks[idx] || initialValue;
     const setValue = newValue => {
-      value = newValue;
+      hooks[idx] = newValue;
     };
+    idx++;
     return [value, setValue];
   }
-  return { useState };
+  // useEffect
+  function useEffect(callback, dep) {
+    const oldDep = deps[idx];
+    let hasChanged = true;
+    if (oldDep) {
+      hasChanged = dep.some((d, i) => !Object.is(d, oldDep[i]));
+    }
+    if (hasChanged) callback();
+    deps[idx] = dep;
+  }
+
+  function render(Component) {
+    const result = Component();
+    result.render();
+    idx = 0;
+    return result;
+  }
+
+  return { useState, useEffect, render };
 })();
+
+function Component() {
+  const [count, setCount] = React.useState(0);
+  return {
+    render: () => console.log('render count', count),
+    click: () => setCount(count + 1)
+  };
+}
